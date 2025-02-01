26ece5db38b4a454ba040df96993fc20a2235235
imjorgemor <imjorgemor@outlook.es>
2025-02-01 23:13:07 +0100

    unregister test

--- src/__tests__/unregister.test.ts
+++ src/__tests__/unregister.test.ts
@@ -1,6 +1,4 @@
-import React from 'react';
-import { render, screen, renderHook, act } from "@testing-library/react";
-import userEvent from '@testing-library/user-event';
+import { renderHook, act } from "@testing-library/react";
 
 import { useFormHook } from "../useFormHook";
 
